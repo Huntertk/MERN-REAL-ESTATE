5d221f011a53fd93c57cdadad3a39de16704b199
MD TAUFIK <110840076+Huntertk@users.noreply.github.com>
2023-10-29 21:03:18 +0530

    created sing and api route and test

--- api/index.js
+++ api/index.js
@@ -1,14 +1,20 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
-import userRouter from './routes/user.route.js'
 dotenv.config()
 
+//Router
+import userRouter from './routes/user.route.js'
+import authRouter from './routes/auth.route.js'
+
 const app = express()
+
+app.use(express.json())
 mongoose.connect(process.env.MONGO)
 .then(() => console.log("Connected to MongoDB")).catch((err) => console.log(err) )
 
 app.use("/api/user", userRouter)
+app.use("/api/auth", authRouter)
 
 app.listen(3000, () => {
     console.log("Server is Running on port 3000!!!");

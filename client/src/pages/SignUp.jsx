34a6ebddf016922507fc3487a83ee47adb6ab9c5
MD TAUFIK <110840076+Huntertk@users.noreply.github.com>
2023-10-30 17:43:23 +0530

    completed sign up page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,11 +1,53 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 const SignUp = () => {
+  const [formData, setFormData] = useState({})
+  const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const navigate = useNavigate()
+
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value
+    })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    try {
+      setLoading(true)
+      const res = await fetch('/api/auth/signup', {
+        method:'POST',
+        headers:{
+          'Content-Type':'application/json',
+        },
+        body: JSON.stringify(formData),
+      })
+      const data = await res.json()
+      if(data.success === false){
+        setError(data.message);
+        setLoading(false);
+        return;
+      }
+      setLoading(false)
+      setError(null)
+      navigate('/sign-in')
+      console.log(data);
+    } catch (err) {
+      setError(err.message)
+      setLoading(false);
+    }
+    
+  }
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
-      <form className='flex flex-col gap-4 '>
+      <form className='flex flex-col gap-4 ' onSubmit={handleSubmit}>
         <input 
         type="text" 
         id='username'
@@ -13,6 +55,7 @@ const SignUp = () => {
         className='border p-3 rounded-lg' 
         required
         autoComplete='off'
+        onChange={handleChange}
         />
         <input 
         type="email" 
@@ -21,6 +64,7 @@ const SignUp = () => {
         className='border p-3 rounded-lg' 
         required
         autoComplete='off'
+        onChange={handleChange}
         />
         <input 
         type="password" 
@@ -29,10 +73,12 @@ const SignUp = () => {
         className='border p-3 rounded-lg' 
         required
         autoComplete='off'
+        onChange={handleChange}
         />
         <button 
         className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-80 disabled:cursor-not-allowed'
-        >Sign Up</button>
+        disabled={loading}
+        >{loading ? "Loading..." : "Sign Up"}</button>
       </form>
       <div className="flex gap-2 mt-5">
         <p>Have an account ? </p>
@@ -40,6 +86,7 @@ const SignUp = () => {
           <span className='text-blue-700 hover:underline'>Sign in</span>
         </Link>
       </div>
+      {error && <p className="text-red-500 mt-5">{error}</p> }
     </div>
   )
 }

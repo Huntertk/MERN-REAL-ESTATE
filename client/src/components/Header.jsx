79e8d4c85b362eee37e55f1ec7f92aba1740251f
MD TAUFIK <110840076+Huntertk@users.noreply.github.com>
2023-10-31 18:24:08 +0530

    Making Profile Protected

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import {FaSearch} from 'react-icons/fa'
 import { Link } from 'react-router-dom'
+import {useSelector} from 'react-redux'
 
 const Header = () => {
+    const {currentUser} = useSelector(state => state.user)
   return (
     <header className='bg-slate-200 shadow-md'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -30,8 +32,12 @@ const Header = () => {
                     <li className='hidden sm:inline text-slate-700 hover:underline cursor-pointer'>About</li>
                 </Link>
 
-                <Link to="/sign-in">
-                    <li className='text-slate-700 hover:underline cursor-pointer'>Sign in</li>
+                <Link to={currentUser ? "/profile" :"/sign-in"}>
+                    {
+                        currentUser ? (
+                            <img className='rounded-full h-7 w-7 object-cover' src={currentUser.avatar} alt={currentUser.username} />
+                        ) : <li className='text-slate-700 hover:underline cursor-pointer'>Sign in</li>
+                    }
                 </Link>
             </ul>
         </div>
